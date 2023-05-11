fc06da3ebf6bc20871ff8576e8c6a4d68a3c6c19
den <katatsu12@gmail.com>
2023-05-11 18:50:34 +0300

    Test loop in Svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,29 @@
 <script>
-	export let name;
+	export let name = 'Den';
+	let people = [{ name: 'John', age: 21 }, { name: 'Bob', age: 21 }, { name: 'Jame', age: 30 }];
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<input type="text" bind:value={name} >
+
+	<ul>
+		{#each people as person}
+			<li>{person.name}: {person.age}</li>
+		{/each}
+    </ul>
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
+		color: #5d9497;
 		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		font-weight: bold;
 	}
 </style>
