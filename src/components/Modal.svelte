157dea1884fe47e2d587adaf4ffc5bc2c3991357
den <katatsu12@gmail.com>
2023-05-13 21:39:48 +0300

    Styles for modal component

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -10,4 +10,26 @@
 	</div>
 {/if}
 
-<style></style>
+<style>
+	.backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  .modal {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 300px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    padding: 20px;
+  }
+</style>
