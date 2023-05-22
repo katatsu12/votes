ea64f38f3fd59a4fa39b68fb66790226cdac7782
den <katatsu12@gmail.com>
2023-05-22 22:09:29 +0300

    Update footer links

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,8 @@
 <footer>
   <div class="links">
     <a target="_blank" href="https://github.com/katatsu12">GitHub</a>
+    <a target="_blank" href="mailto:katatsu12@gmail.com">Email me</a>
+    <a target="_blank" href="https://www.linkedin.com/in/den-zubritskiy-97aa38169/">LinkedIn</a>
   </div>
 </footer>
 
@@ -14,5 +16,6 @@
     display: flex;
     justify-content: center;
     align-items: center;
+    gap: 20px;
   }
 </style>
